a1a113ed4dfc8114981f86d49760b6364b977b16
Laion Pereira <kingslaion@gmail.com>
2023-01-18 11:27:57 -0300

    continuação do projeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const jogadoresCraques = []
+const jogadores = []
 const obj1 = {
 nome: "Endrick",
 idade: 16,
@@ -36,149 +36,24 @@ imagem: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/http
 site: "https://pt.wikipedia.org/wiki/Jude_Bellingham"
 }
 
-jogadores.push(obj1, obj2, obj3, obj4);
 
-console.log(jogadoresCraques);
 
-/*
-[
-{
-nome: "Endrick",
-idade: 16,
-posicao: "Atacante",
-timeAtual: "Palmeiras FC",
-pais: "Brasil",
-imagem: "https://www.psg.fr/sites/default/files/styles/image_equipe_article/public/images/2021-02/endrick-psg.jpg?itok=6pFoJgKv",
-site: "https://www.psg.fr/equipe/endrick-3"
-},
-{
-nome: "Haaland",
-idade: 21,
-posicao: "Atacante",
-timeAtual: "Borussia Dortmund",
-pais: "Noruega",
-imagem: "https://www.bvb.de/globalassets/images/players/haaland-erling-braut/2020-2021/erling-haaland-bvb-2020-2021-portrait-bvb.jpg",
-site: "https://www.bvb.de/de/teams/profis/erling-haaland"
-},
-{
-nome: "Mbape",
-idade: 24,
-posicao: "Atacante",
-timeAtual: "Paris Saint-Germain",
-pais: "França",
-imagem: "https://www.psg.fr/sites/default/files/styles/image_equipe_article/public/images/2021-02/mbappe-psg.jpg?itok=1Y0Ysc-L",
-site: "https://www.psg.fr/equipe/mbappe-7"
-},
-{
-nome: "Bellingham",
-idade: 19,
-posicao: "Meio-campista",
-timeAtual: "Borussia Dortmund",
-pais: "Inglaterra",
-imagem: "https://www.bvb.de/globalassets/images/players/bellingham-jude/2020-2021/jude-bellingham-bvb-2020-2021-portrait-bvb.jpg",
-site: "https://www.bvb.de/de/teams/profis/jude-bellingham"
+const jogadoresCraques = [obj1, obj2, obj3, obj4];
+
+
+for (let i = 0; i < jogadoresCraques.length; i++) {
+  const jogador = jogadoresCraques[i];
+  const container = document.createElement("div");
+  container.innerHTML = `
+    <img src="${jogador.imagem}" alt="${jogador.nome}">
+    <h2>${jogador.nome}</h2>
+    <p>Idade: ${jogador.idade}</p>
+    <p>Posição: ${jogador.posicao}</p>
+    <p>Time atual: ${jogador.timeAtual}</p>
+    <p>País: ${jogador.pais}</p>
+    <a href="${jogador.site}">Saiba mais</a>
+  `;
+  document.body.appendChild(container);
 }
-]
-*/
-
-let jogadoresCraque = []
-jogadoresCraques.push(obj1,obj2,obj3,obj4)
-
-for (let i =0;i<jogadoresCraques.length; i++){
-if(jogadoresCraques[i].pais==="Brasil"){
-jogadoresBrasileiros.push(jogadoresCraques[i])
-}
-}
-
-for(let i=0;i< jogadoresCraques.length;i++){
-console.log(jogadoresCraques[i].posicao)
-}
-
-function jogadoresRelatorio(objeto){
-console.log(`Nome:${objeto.nome.toUpperCase()}\nIdade:${objeto.idade}\nPosição:${objeto.posicao}\nTime Atual:${objeto.timeAtual}\nPaís:${objeto.pais}\n`)
-}
-for (let i=0;i<jogadoresCraques.length;i++){
-jogadoresRelatorio(jogadoresCraques[i]);
-};
-
-function buscarJogadores (jogadoresCraques){
-const nome = prompt("Digite o nome do jogador").toUpperCase()
-let objeto = jogadoresCraques.filter(
-jogador=>{
-return jogador.nome.toUpperCase().includes(nome)
-}
-)
-if(objeto.length===0){
-alert("Jogador não encontrado!")
-}else{
-console.log("Achouuu!!!!")
-return objeto
-}
-}
-
-
-
-function criarCard (obj){
-    const card = document.createElement("div")
-    const imagem = document.createElement("img")
-    const nome = document.createElement("a")
-    const dados = document.createElement("ul")
-    const idade = document.createElement("li")
-    const posicao = document.createElement("li")
-    const timeAtual = document.createElement("li")
-    const pais = document.createElement("li")
-
-    imagem.src = obj.imagem
-    nome.innerText = obj.nome
-    nome.href = obj.site
-    nome.target = "_blank"
-    idade.innerText = `Idade : ${obj.idade}`
-    posicao.innerText = `Posição: ${obj.posicao}`
-    timeAtual.innerText = `Time Atual: ${obj.timeAtual}`
-    pais.innerText = `País: ${obj.pais}`
-    
-    card.className = "card"
-    dados.className = "texto"
-     
-    dados.append(nome, idade, posicao, timeAtual, pais)
-    card.append(imagem, dados)
-    
-    return card
-    
-}
-
-
-function renderizarProds (list) {
-    const secaoProds = document.getElementById("secaoProds")
-    secaoProds.innerHTML = ""
-    console.log(list)
-    list.forEach(produto => {
-        const cardProdCriado = criarCard(jogadoresCraques)
-        secaoProds.appendChild(cardProdCriado)
-    })
-    
-}
-
-renderizarProds(jogadoresCraques)
-
-
-function pesquisarJogadores() {
-    const inputPesquisa = document.getElementById("txtBusca")
-    const botaoPesquisa = document.getElementById("btnBusca")
-
-    botaoPesquisa.addEventListener("click", () => {
-        const valor = inputPesquisa.value.toUpperCase()
-        let filtro = jogadoresCraques.filter(jogador => jogador.nome.toUpperCase().includes(valor))
-        
-        if(filtro.length) {
-            renderizarProds(filtro)
-        } else {
-            alert("Jogador não encontrado")
-        }
-    })
-}
-
-pesquisarJogadores()
-
 
 
